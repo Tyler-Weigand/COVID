f043ed40ffb3e870dbeab3a1d33b2018a618c1e1
Tyler-Weigand <tweigand1@hwemail.com>
2020-11-30 13:14:15 -0800

    made reading the file

--- src/CoronaSolver.java
+++ src/CoronaSolver.java
@@ -1,8 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class CoronaSolver {
-	public static void main(String [] args) {
-		int days;
-		int flight;
-		int[] LA;
-		int[] NYC;
+	public String solve(String inputFileName) throws IOException{
+		File text = new File(inputFileName);
+		Scanner s = new Scanner(text);
+		
+		int days = s.nextInt();
+		int flight = s.nextInt();
+		int[] LA = new int[days];
+		int[] NYC = new int[days];
+		
+		for(int i = 0; i < days; i++) {
+			LA[i] = s.nextInt();
+			NYC[i] = s.nextInt();
+		}
+		
+		return "";
 	}
 }
